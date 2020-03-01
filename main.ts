b9ac2ead5df0a0305c85882a0e127525fc25c657
jonmarin <jonsonmarin@gmail.com>
2020-02-29 22:25:45 -0500

    Made the code changes for a doorbell.

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+input.onButtonPressed(Button.A, function () {
+    music.beginMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.B, function () {
+    music.beginMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once)
+})
